80feeff90a33638e76bfff08217504613cbecb0c
davidkatz <15Dkatz@shcp.edu>
2016-10-31 09:40:30 -0700

    complete step 7

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,29 +1,39 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable no-console */
+/* eslint-disable import/no-extraneous-dependencies, no-console */
+
 import gulp from 'gulp';
 import babel from 'gulp-babel';
-import del from 'del';
-import { exec } from 'child_process';
 import eslint from 'gulp-eslint';
+import del from 'del';
+import webpack from 'webpack-stream';
+import webpackConfig from './webpack.config.babel';
 
 const paths = {
   allSrcJs: 'src/**/*.js',
+  serverSrcJs: 'src/server/**/*.js?(x)',
+  sharedSrcJs: 'src/shared/**//*.js?(x)',
+  clientEntryPoint: 'src/client/app.js',
   gulpFile: 'gulpfile.babel.js',
+  webpackFile: 'webpack.config.babel.js',
   libDir: 'lib',
+  distDir: 'dist',
+  clientBundle: 'dist/client-bundle.js?(map)',
 };
-
-gulp.task('clean', () => del(paths.libDir));
 
 gulp.task('lint', () =>
   gulp.src([
     paths.allSrcJs,
     paths.gulpFile,
+    paths.webpackFile,
   ])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
 );
 
+gulp.task('clean', () => del([
+  paths.libDir,
+  paths.clientBundle,
+]));
 
 gulp.task('build', ['lint', 'clean'], () =>
   gulp.src(paths.allSrcJs)
@@ -31,11 +41,10 @@ gulp.task('build', ['lint', 'clean'], () =>
     .pipe(gulp.dest(paths.libDir))
 );
 
-gulp.task('main', ['build'], (callback) => {
-  exec(`node ${paths.libDir}`, (error, stdout) => {
-    console.log(stdout);
-    return callback(error);
-  });
+gulp.task('main', ['lint', 'clean'], () => {
+  gulp.src(paths.clientEntryPoint)
+    .pipe(webpack(webpackConfig))
+    .pipe(gulp.dest(paths.distDir));
 });
 
 gulp.task('watch', () => {
